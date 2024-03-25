e02888d417fb908b68439742d926f3cf3386ee1a
yuta <yuta0525ny@gmail.com>
2024-03-25 16:02:29 +0900

    refactor(WIP) OLEDの関数分割した

--- lib/OLED_a/OLED_a.h
+++ lib/OLED_a/OLED_a.h
@@ -43,11 +43,59 @@ class oled_attack{
     int goDir;  //loop関数ないしか使えないangle go_ang.degressの値をぶち込んでグローバルに使うために作った税
     void setup();
     void OLED();
+    int display_main();
+    int display_start();
+    int display_selectColor();
+    int display_waitStart();
+    int set_Line_Threshold();
+    int display_Line();
+    int set_getBall_Threshold();
+    int display_Ball();
+    int set_MotorVal();
+    int set_Avaliable();
+    int display_Cam();
+    int display_getBall();
+    int Kick_test();
+
+    int check_TactSwitch();
     int Button_selectCF = 0;  //コートの方向を決めるときに特殊なことをするので、セレクト変数を変えときますぜよ
+    int Sentor_A = 0;
     int Left_A = 0;
     int Right_A = 0;
+
+    int Left;
+    int Sentor;
+    int Right;
     timer Left_t;
+    timer Sentor_t;
     timer Right_t;
+
+    int A = 0;
+    int B = 999;  //ステート初期化のための変数
+    int aa = 0;  //タクトスイッチのルーレット状態防止用変数
+
+    int flash_OLED = 0;  //ディスプレイの中で白黒点滅させたいときにつかう
+    int OLED_select = 1;  //スイッチが押されたときにどこを選択しているかを示す変数(この数字によって選択画面の表示が変化する)
+    int Button_select = 0;  //スイッチが押されたときにどこを選択しているかを示す変数(この数字によってexitかnextかが決まる)
+
+    int OLED_ball_x = 0;
+    int OLED_ball_y = 0;
+
+    int OLED_cam_x = 0;
+    int OLED_cam_y = 0;
+
+    float line_x = 0;
+    float line_y = 0;
+
+    float Ax = 0;
+    float Ay = 0;
+    float Bx = 0;
+    float By = 0;
+
+    int OLED_line_ax = 0;
+    int OLED_line_ay = 0;
+    int OLED_line_bx = 0;
+    int OLED_line_by = 0;
 
     int check_select = 0;
     int check_flag = 0;
